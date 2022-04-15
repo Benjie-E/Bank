9a7c87645cfe00ba0da66024bd84f9a4e14dd6ca
Benjie-E <90060865+Benjie-E@users.noreply.github.com>
2022-04-15 14:59:27 -0400

    initial commit

---
+++ Bank-Starter/ui.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+int GetNumberFromUser(int minValue, int maxValue);
+string GetAccountNumberFromUser();
+float GetCurrencyFromUser();
+bool LoginUser();
+void AddAccount();
+void DeleteAccount();
+void UpdateAccount();
+void ViewAccount();
+void Deposit();
+void Withdraw();
+void AccountMenu();
+void MainMenu();
+
+std::string SafeGetLine();
